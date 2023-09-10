2a5ce920da8d2beb845f0e6308550d8d46c5582f
yasircs <yasir.arfatcs@gmail.com>
2023-09-10 16:00:28 +0500

    copy-removeSpace-clearText

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,17 +14,34 @@ export default function TextForm(props) {
     let newtext=text.toLocaleLowerCase();
     setText(newtext);
   }
+  const handleClearClick=()=>{
+    console.log("Uppercase was click");
+    let newtext='';
+    setText(newtext);
+  }
   const handleOnChange=(event)=>{
     console.log("OnChange was click")
     setText(event.target.value)
+  }
+  const handleCopy=(event)=>{
+    let text=document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+  const handleSpaces=(event)=>{
+    let newtext=text.split(/[ ]+/);
+    setText(newtext.join(" "))
   }
   return (
     <div>
     <h1>{props.heading}</h1>
    <div className="form-group">
-    <textarea class="form-control" id="textarea1" value ={text} onChange={handleOnChange} rows="8"></textarea>
+    <textarea  class="form-control" id="myBox" value ={text} onChange={handleOnChange} rows="8"></textarea>
     <button className="btn btn-primary my-2 mx-1 "onClick={handleUpClick}>Convert to UpperCase</button>
     <button className="btn btn-primary my-2 mx-1 "onClick={handleLoClick}>Convert to LowCase</button>
+    <button className="btn btn-primary my-2 mx-1 "onClick={handleClearClick}>ClearText</button>
+    <button className="btn btn-primary my-2 mx-1 "onClick={handleCopy}>Copy Text</button>
+    <button className="btn btn-primary my-2 mx-1 "onClick={handleSpaces}>Remove Spaces</button>
 
   </div>
   <div className="container my-2">
