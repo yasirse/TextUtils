746514cb29a9a2d8679d27690758f33d9700325d
yasircs <yasir.arfatcs@gmail.com>
2023-09-12 22:00:04 +0500

    alert for dark mode created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+import { type } from "@testing-library/user-event/dist/type";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
+import React,{useState} from 'react'
+
 
 
     function App() 
     {
+      const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+      const [alert, setAlert] = useState(null);
+      const toggleMode=()=>
+      {
+        if(mode==='light')
+        {
+          setMode('dark');
+          document.body.style.backgroundColor='#082544';
+          showAlert(" Light has been enabled","success");
+        }
+        else
+        {
+          setMode('light');
+          document.body.style.backgroundColor='white';
+          showAlert(" Light has been enabled","success");
+        }
+      }
+      const showAlert=(message,type)=>
+      {
+        setAlert({msg:message,
+          type:type
+        })
+        setTimeout(()=>{setAlert(null);},1500);
+      }
+
       return (
       <>
-      <Navbar title="TextUtils" abouttext="AboutTextutils"/>
+      <Navbar title="TextUtils" abouttext="AboutTextutils" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert}/>
       <div className="container my-3">
-        <TextForm heading="Enter the text here to analyse"/>
+        <TextForm showAlert={showAlert} heading="Enter the text here to analyse"mode={mode}/>
       </div>    
       </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -8,11 +8,13 @@ export default function TextForm(props) {
     console.log("Uppercase was click");
     let newtext=text.toUpperCase();
     setText(newtext);
+    props.showAlert("Converted to Uppercase","success");
   }
   const handleLoClick=()=>{
     console.log("Uppercase was click");
     let newtext=text.toLocaleLowerCase();
     setText(newtext);
+    props.showAlert("Converted to Loqwecase","success");
   }
   const handleClearClick=()=>{
     console.log("Uppercase was click");
@@ -27,30 +29,32 @@ export default function TextForm(props) {
     let text=document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    props.showAlert("Text Copied","success");
   }
   const handleSpaces=(event)=>{
     let newtext=text.split(/[ ]+/);
-    setText(newtext.join(" "))
+    setText(newtext.join(" "));
+    props.showAlert("Spaces removed","success");
   }
   return (
-    <div>
-    <h1>{props.heading}</h1>
-   <div className="form-group">
-    <textarea  class="form-control" id="myBox" value ={text} onChange={handleOnChange} rows="8"></textarea>
-    <button className="btn btn-primary my-2 mx-1 "onClick={handleUpClick}>Convert to UpperCase</button>
-    <button className="btn btn-primary my-2 mx-1 "onClick={handleLoClick}>Convert to LowCase</button>
-    <button className="btn btn-primary my-2 mx-1 "onClick={handleClearClick}>ClearText</button>
-    <button className="btn btn-primary my-2 mx-1 "onClick={handleCopy}>Copy Text</button>
-    <button className="btn btn-primary my-2 mx-1 "onClick={handleSpaces}>Remove Spaces</button>
-
-  </div>
-  <div className="container my-2">
-    <h1>Your text summary</h1>
-    <p>{text.split(" ").length} words and {text.length} characters</p>
-    <p>{0.008*text.split(" ").length} time to read words</p>
-    <h2>Preview</h2>
-    <p>{text}</p>
-  </div>
+    <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
+      <h1>{props.heading}</h1>
+      <div className="form-group">
+        <textarea  class="form-control" id="myBox" value ={text} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}} onChange={handleOnChange} rows="8"></textarea>
+        <button className="btn btn-primary my-2 mx-1 "onClick={handleUpClick}>Convert to UpperCase</button>
+        <button className="btn btn-primary my-2 mx-1 "onClick={handleLoClick}>Convert to LowCase</button>
+        <button className="btn btn-primary my-2 mx-1 "onClick={handleClearClick}>ClearText</button>
+        <button className="btn btn-primary my-2 mx-1 "onClick={handleCopy}>Copy Text</button>
+        <button className="btn btn-primary my-2 mx-1 "onClick={handleSpaces}>Remove Spaces</button>
+        </div>
+        
+        <div className="container my-2">
+          <h1>Your text summary</h1>
+          <p>{text.split(" ").length} words and {text.length} characters</p>
+          <p>{0.008*text.split(" ").length} time to read words</p>
+          <h2>Preview</h2>
+          <p>{text.length>0?text:"Enter something for preview"}</p>
+        </div>
     </div>
   )
 }
